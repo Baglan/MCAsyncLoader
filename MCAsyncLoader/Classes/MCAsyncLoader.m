2c1a57d045561a1c74bd4704275cc0e3f5539a4d
Baglan Dosmagambetov <baglan.dos@gmail.com>
2012-10-03 21:02:57 +0600

    - Initial changes

---
+++ MCAsyncLoader/Classes/MCAsyncLoader.m
@@ -0,0 +1,83 @@
+//
+//  MCAsyncLoader.m
+//  MCAsyncLoader
+//
+//  Created by Baglan on 10/3/12.
+//  Copyright (c) 2012 MobileCreators. All rights reserved.
+//
+
+#import "MCAsyncLoader.h"
+
+@interface MCAsyncLoader () {
+    NSCache *_imageCache;
+}
+
+@end
+
+@implementation MCAsyncLoader
+
+// Sound board singleton
+// Taken from http://lukeredpath.co.uk/blog/a-note-on-objective-c-singletons.html
++ (MCAsyncLoader *)sharedInstance
+{
+    __strong static id _sharedObject = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedObject = [[self alloc] init];
+    });
+    return _sharedObject;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _imageCache = [[NSCache alloc] init];
+    }
+    return self;
+}
+
++ (UIImage *)renderImage:(UIImage *)image
+{
+    UIGraphicsBeginImageContext(image.size);
+    CGRect rect = CGRectMake(0, 0, image.size.width, image.size.height);
+    [image drawInRect:rect];
+    UIImage *renderedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return renderedImage;
+}
+
+- (void)loadAndPrerenderImageFromURL:(NSURL *)url forKey:(id)key
+{
+    dispatch_queue_t downloadQueue = dispatch_queue_create("MCAsyncLoader:loadAndPrerenderImageFromURL", NULL);
+    
+    dispatch_async(downloadQueue, ^(void) {
+        UIImage *prerenderedImage = nil;
+        
+        prerenderedImage = [_imageCache objectForKey:url];
+        if (prerenderedImage == NULL) {
+            NSData *data = [NSData dataWithContentsOfURL:url];
+            UIImage *image = [UIImage imageWithData:data];
+            prerenderedImage = [self.class renderImage:image];
+        }
+        
+        if (prerenderedImage) {
+            dispatch_async(dispatch_get_main_queue(), ^(void) {
+                [[NSNotificationCenter defaultCenter] postNotificationName:MC_ASYNC_LOADER_IMAGE_LOADED_AND_PRERENDERED_NOTIFICATION
+                                                                    object:key
+                                                                  userInfo:@{@"image" : prerenderedImage}];
+            });
+        }
+        
+        [_imageCache setObject:prerenderedImage forKey:url];
+    });
+    
+    // dispatch_release(downloadQueue);
+}
+
++ (void)loadAndPrerenderImageFromURL:(NSURL *)url forKey:(id)key
+{
+    [[self sharedInstance] loadAndPrerenderImageFromURL:url forKey:key];
+}
+
+@end
--- MCAsyncLoader/ViewController.m
+++ MCAsyncLoader/ViewController.m
@@ -7,17 +7,54 @@
 //
 
 #import "ViewController.h"
+#import "MCAsyncLoader.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    NSMutableDictionary *_imageViews;
+}
 
 @end
 
+#define NUMBER_OF_IMAGES        20
+#define IMAGE_URL_FORMAT        @"http://mobicreators.com/experiments/MCAsyncLoader/%d.jpg"
+#define PLACEHOLDER_IMAGE_NAME  @"placeholder.jpg"
+
 @implementation ViewController
+
+- (void)onImageLoaded:(NSNotification *)loadedNotification
+{
+    UIImageView *imageView = [_imageViews objectForKey:loadedNotification.object];
+    UIImage *image = [loadedNotification.userInfo objectForKey:@"image"];
+    imageView.image = image;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    CGSize imageViewSize = _scrollView.frame.size;
+    
+    // Set content size
+    CGSize contentSize = _scrollView.frame.size;
+    contentSize.width = contentSize.width * NUMBER_OF_IMAGES;
+    _scrollView.contentSize = contentSize;
+    
+    // Notifications
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onImageLoaded:) name:MC_ASYNC_LOADER_IMAGE_LOADED_AND_PRERENDERED_NOTIFICATION object:nil];
+    
+    // Add images
+    _imageViews = [NSMutableDictionary dictionary];
+    for (int i=0; i<NUMBER_OF_IMAGES; i++) {
+        UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:PLACEHOLDER_IMAGE_NAME]];
+        [imageView setFrame:CGRectMake(imageViewSize.width * i, 0, imageViewSize.width, imageViewSize.height)];
+        [_scrollView addSubview:imageView];
+        imageView.backgroundColor = [UIColor redColor];
+        id key = [NSString stringWithFormat:@"%d", i];
+        [_imageViews setObject:imageView forKey:key];
+        NSURL *imageURL = [NSURL URLWithString:[NSString stringWithFormat:IMAGE_URL_FORMAT, i]];
+        [MCAsyncLoader loadAndPrerenderImageFromURL:imageURL forKey:key];
+    }
 }
 
 - (void)didReceiveMemoryWarning
