7eac1caa712d42dc880d1845d54636fb016960d9
Baglan Dosmagambetov <baglan.dos@gmail.com>
2012-10-03 10:40:05 +0600

    Initial Commit

---
+++ MCAsyncLoader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCAsyncLoader
+//
+//  Created by Baglan on 10/3/12.
+//  Copyright (c) 2012 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
